6b91609c0e29b31b759ea95013c012eed09e8543
Bala Luguja <bluguja@gmail.com>
2020-10-22 20:38:22 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,52 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+  get '/reversename/:name' do 
+    #binding.pry 
+    params["name"].reverse
+  end
 
+  get '/square/:number' do 
+    #binding.pry
+    variable= params["number"].to_i*params["number"].to_i
+    variable.to_s
+  end 
+
+  get '/say/:number/:phrase' do
+    #binding.pry
+    answer = ''
+    variable_number = params["number"].to_i
+    variable_number.times do
+      answer += params[:phrase] 
+    end
+    answer
+  end
+
+   get '/say/:word1/:word2/:word3/:word4/:word5' do
+   #binding.pry
+  # params["word1"] + " " + params["word2"] + " " + params["word3"] + " " + params["word4"] + " " + params["word5"] + "."
+  "#{params["word1"]}" + " " + "#{params["word2"]}" + " " + "#{params["word3"]}" + " " + "#{params["word4"]}" + " " + "#{params["word5"]}" "."
+  end
+ 
+    get '/:operation/:number1/:number2' do
+      #binding.pry
+      # @result = params[:num1].to_i * params[:num2].to_i
+      # "#{@result}"
+        @operation = params[:operation]
+        @num1 = params[:number1].to_i
+        @num2 = params[:number2].to_i
+        case @operation
+          when "add"
+          "#{@num1 + @num2}"
+         when "subtract"
+          "#{@num2 - @num1}"
+         when "multiply"
+          "#{@num1 * @num2}"
+         when "divide"
+          "#{@num1 / @num2}"
+        end 
+      end 
+    
 end
+
+  
